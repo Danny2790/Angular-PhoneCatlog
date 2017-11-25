1127b56c9fdd2311c2e4fe1db61ad2dba691a965
Akash Danao <akash@aitglobalinc.com>
2017-11-26 02:22:02 +0530

    use http service to get mock phone json and show in the ui

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,32 +1,15 @@
 angular.module("phonecatApp")
     .component("phoneList", {
         templateUrl : "./phone-list/phone-list.html",
-        controller : function phoneListController(){
-            this.phones = [
-                {
-                     name : "nexus s",
-                     snippet : "Fast just got faster with Nexus S",
-                     age : 1
-                },
-                {
-                    name : "Motorola ",
-                    snippet : "Moto e",
-                    age : 2
-                },
-                {
-                    name : "Nokia ",
-                    snippet : "nokie at his best",
-                    age : 3
-                },
-                {
-                    name : "AAA",
-                    snippet : "AA worlds leader",
-                    age : 1
-                }
-             ]
-            this.orderProp = 'age';
+        controller : ['$http', function phoneListController($http){
+            var self = this; //creat an alias of controller instance
+            $http.get('phones/phones.json').then( function (response){
+                self.phones = response.data;                
+            });
+            self.orderProp = 'age';
         }
-    });
+    ]
+});
 
 
             
