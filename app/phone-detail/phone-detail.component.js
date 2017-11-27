9858dddde21cccfb66af11c378bdf79c65f5fafa
Akash Danao <akash@aitglobalinc.com>
2017-11-27 16:43:51 +0530

    add runtimechange of images on click thumbnaikls

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -4,9 +4,12 @@ angular.module("phonecatApp")
         controller :[ '$routeParams', '$http', function phoneDetailController($routeParams, $http){
                 //this.phoneId = $routeParams.phoneId;
                 var self = this;
-
+                self.setImage = function setImage(image){
+                    self.mainImageUrl = image;
+                }
                 $http.get('/phones/' + $routeParams.phoneId + '.json').then(function(response){
                     self.phone = response.data;
+                    self.setImage(self.phone.images[0]);
                 });
             }
         ]
